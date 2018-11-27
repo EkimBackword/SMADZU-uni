d3933c4f278401d438a20ba1d107384b828b7144
MikhailZharkov <zharkov@abcreg.ru>
2018-11-27 23:20:33 +0300

    11 + 12

--- 12/12.js
+++ 12/12.js
@@ -1,42 +1,40 @@
-…
-#include<stdlib.h>//Библиотека для работы с псевдослучайными числами
-#include<math.h>//Библиотека для работы с математическими функциями
-...
-const int N;     //Количество точек сигнала
-double x[N], y[N];      //Массивы координатточек сигнала
 
-double H1, H2;//Значения амплитуд двух синусоид
-double lambda1, lambda2;//Значения частот двух синусоид
-double e;//Уровень шума
-using namespace std;
-...
-double dx = 2*3.141593/(N - 1);//Величина шага изменения координаты x
+const fs = require("fs");
+fs.writeFileSync("12.dat", ""); 	// Запись в файл строки со структурой обучающих примеров
 
-srand((unsigned)time_t(0));                 //Инициализация генератора псевдослучайных чисел
+const n = 9;						// > 8
+const N = Math.pow(2, n);     		// Количество точек сигнала
+let x = [], y = [];      			// Массивы координатточек сигнала [N]
 
-for (int i = 0; i < int(N/3.0); i++)         //Моделирование первой части сигнала
+let H1 = 1, H2 = 0.6;				// Значения амплитуд двух синусоид
+let lambda1 = 3.5, lambda2 = 40;	// Значения частот двух синусоид
+let e = 0.2;						// Уровень шума
+
+let dx = 2*3.141593/(N - 1); 		// Величина шага изменения координаты x
+
+
+for (let i = 0; i < Math.floor(N/3.0); i++)         //Моделирование первой части сигнала
 {
 	x[i] = i*dx;
-	y[i] = H1*sin(lambda1*x[i]) + e*(rand()/(double)RAND_MAX - 0.5);
+	y[i] = H1*Math.sin(lambda1*x[i]) + e*(Math.random() - 0.5);
 }
 
-for (int i = int(N/3.0); i < int(2*N/3.0); i++)     //Моделирование второй части сигнала
+for (let i = Math.floor(N/3.0); i < Math.floor(2*N/3.0); i++)     //Моделирование второй части сигнала
 {
 	x[i] = i*dx;
-	y[i] = H1*sin(lambda1*x[i]) + H2*sin(lambda2*x[i]) +
-		e*(rand()/(double)RAND_MAX -0.5);
+	y[i] = H1*Math.sin(lambda1*x[i]) + H2*Math.sin(lambda2*x[i]) +
+		e*(Math.random() -0.5);
 }
 
-for (int i = int(2*N/3.0); i < N; i++)                //Моделирование третьей части сигнала
+for (let i = Math.floor(2*N/3.0); i < N; i++)                //Моделирование третьей части сигнала
 {
 	x[i] = i*dx;
-	y[i] = H1*sin(lambda1*x[i]) + e*(rand()/(double)RAND_MAX - 0.5);
+	y[i] = H1*Math.sin(lambda1*x[i]) + e*(Math.random() - 0.5);
 }
-	ofstream ofs("d_a.dat");                               //Открытие файла для записи
 
-for (int i = 0; i < N; i++)
+for (let i = 0; i < N; i++)
 {
-	ofs<<y[i]<<std::endl;                   //Сохранение в файл y-координат сигнала
+	fs.appendFileSync("12.dat", `${y[i]}\r\n`);
+	// ofs<<y[i]<<std::endl;                   //Сохранение в файл y-координат сигнала
 }
 
-ofs.close();                                                   //Закрытие файла
