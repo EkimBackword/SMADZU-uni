1e19dfd28062e75512049b3a5e5c0d32afe832e3
Жарков Михаил <zharkov@abcreg.ru>
2018-11-28 18:00:43 +0300

    13 script

--- 13/13.js
+++ 13/13.js
@@ -1,65 +1,90 @@
-…
-#include<stdlib.h>//Библиотека для работы с псевдослучайными числами
-#include<math.h>//Библиотека для работы с математическими функциями
-...
+const fs = require("fs");
+
 //Объявление глобальных переменных
-int n=9;                                 //Степень двойки для расчета N
-int N1 = (1 << n) + 1;  //Количество точек сигнала
+let n=9;                                  //Степень двойки для расчета N
+let N1 = (Math.pow(2, n)) + 1;                             //Количество точек сигнала (Math.pow(2, n)) + 1
+let Depth = 5;                            //Глубина вейвлет-разложения сигнала
+let m_array = [];                         //Массив сигнала [N1]
 
-                
-int Depth;                         //Глубина вейвлет-разложения сигнала
+for (let k = 0; k < N1; k++) {
+  m_array[k] = 0;
+}
 
-double *m_array = new double[N1]; //Массив сигнала  
-					 //Выделение памяти для массива точек
+var text = fs.readFileSync('../12/12.dat','utf8')
+fs.writeFileSync("13.dat", ""); 	// Запись в файл строки со структурой обучающих примеров
+let file_array = text.split('\r\n');
 
-void Decomposition()      //Функция прямого вейвлет-преобразования
+for (let k = 0; k < N1; k++) {
+  m_array[k] = Number(file_array[k]);
+}
+
+let m_array_d = Decomposition(m_array, N1, Depth);
+
+// let m_array_f1 = Filtration(m_array_d, Depth, 0, 10);
+// let m_array_f2 = Filtration(m_array_f1, Depth, 1, 10);
+// let m_array_f3 = Filtration(m_array_f2, Depth, 2, 10);
+
+// let m_array_r = Reconstruction(m_array_f3, N1, Depth);
+
+for (let k = 0; k < N1; k++) {
+  fs.appendFileSync("13-d.dat", `${m_array_d[k]}\r\n`);
+  // fs.appendFileSync("13-f.dat", `${m_array_f1[k]}\r\n`);
+  // fs.appendFileSync("13.dat", `${m_array_r[k]}\r\n`);
+}
+
+
+
+
+
+function Decomposition(m_array, N1, Depth)                    //Функция прямого вейвлет-преобразования
 {
+  //p, q и r являются вспомогательными переменными 
+  let p = Math.pow(2, n-1);                   //двойка в степени (n-1)
+  let q = 2;
+  let r = 1;
 
-  	//p, q и r являются вспомогательными переменными 
-	 int p = 1<<(n - 1);          //двойка в степени (n-1)
-	 int q = 2;
-	 int r = 1;
-
-	for (int lev = 1; lev <= Depth; lev++)         //Цикл последовательного вейвлет-								//разложения
+	for (let lev = 0; lev <= Depth; lev++)  //Цикл последовательного вейвлет-разложения
 	{
-		for (int k = 1; k <= p; k++)
+		for (let k = 0; k <= p; k++)
 		{
-			int temp = (2*k - 1)*r;
-			m_array[temp] = m_array[temp] - 0.5*(m_array[(k - 1)*q] + 												m_array[k*q]);
-    		}
+      let temp = (2*k - 1)*r;
+      m_array[temp] = m_array[temp] - 0.5*(m_array[(k - 1)*q] + m_array[k*q]);
+    }
 
-    		m_array[0] = m_array[0] + 0.5*m_array[r]; //Здесь использовано 							//зеркальное отражение сигнала от границ
+    m_array[0] = m_array[0] + 0.5*m_array[r]; //Здесь использовано зеркальное отражение сигнала от границ
 
-    		m_array[N1 - 1] = m_array[N1 - 1] + 0.5*m_array[N1 - 1 - r];        
-    		for (int k = 1; k < p; k++)
-		{		
-      			m_array[k*q] = m_array[k*q] + 0.25*(m_array[(2*k - 1)*r] + 
-							m_array[(2*k + 1)*r]);
-		}
+    m_array[N1 - 1] = m_array[N1 - 1] + 0.5*m_array[N1 - 1 - r];        
+    for (let k = 0; k < p; k++)
+    {		
+        m_array[k*q] = m_array[k*q] + 0.25*(m_array[(2*k - 1)*r] + m_array[(2*k + 1)*r]);
+    }
 
-    		p = p/2;
-   		 r = q;
-    		q = q*2;
-  	}
+    p = p/2;
+    r = q;
+    q = q*2;
+  }
+
+  return m_array;
 } 
-void Reconstruction()                                //Функция обратного вейвлет-									//преобразования
+
+function Reconstruction(m_array, N1, Depth)                   //Функция обратного вейвлет-преобразования
 {
   //p, r и q являются вспомогательными переменными
-  int p = 1<<(n - Depth);                            //двойка в степени (n-Depth)
-  int r = 1<<(Depth - 1);                             //двойка в степени (Depth-1)
-  int q = r<<1;                                            //двойка в степени r
+  let p = Math.pow(2, n - Depth);                   //двойка в степени (n-Depth)
+  let r = Math.pow(2, Depth - 1);                   //двойка в степени (Depth-1)
+  let q = Math.pow(2, r);                           //двойка в степени r
 
-  for (int lev = Depth; lev > 0; lev--)           //Цикл последовательной вейвлет-сборки
+  for (let lev = Depth; lev > 0; lev--)     //Цикл последовательной вейвлет-сборки
   {
-    for (int k = 1; k < p; k++)
+    for (let k = 1; k < p; k++)
       m_array[k*q] = m_array[k*q] - 0.25*(m_array[(2*k - 1)*r] + m_array[(2*k + 1)*r]);
 
     m_array[0]       = m_array[0] - 0.5*m_array[r];
     m_array[N1 - 1] = m_array[N1 - 1] - 0.5*m_array[N1 - 1 - r];
 
-    for (int k = 1; k <= p; k++)
+    for (let k = 1; k <= p; k++)
     {
-      int temp = (2*k - 1)*r;
+      let temp = (2*k - 1)*r;
       m_array[temp] = m_array[temp] + 0.5*(m_array[(k - 1)*q] + m_array[k*q]);
     }
 
@@ -67,33 +92,35 @@ void Reconstruction()                                //Функция обрат
     q = r;
     r = r/2;
   }
+
+  return m_array;
 }
 
-void Filtration(int layer_numb, double threshold)
+function Filtration(m_array, Depth, layer_numb, threshold)
 {
   //Функция фильтрации одного уровня вейвлет-спектра с номером layer_numb
-//На данном уровне зануляются все вейвлет-коэффициенты,
-//модуль которых меньше значения threshold
-	int k=0;
+  //На данном уровне зануляются все вейвлет-коэффициенты,
+  //модуль которых меньше значения threshold
+	let k=0;
   if (layer_numb <= Depth)
   {
-    int layer_size = 1<<(n - layer_numb);           //двойка в степени (n-layer_numb)
-
-    int t = 1<<(layer_numb - 1);                          //двойка в степени (layer_numb-1)
+    let layer_size = Math.pow(2, n - layer_numb);           //двойка в степени (n-layer_numb)
+    let t =  Math.pow(2, layer_numb - 1);                   //двойка в степени (layer_numb-1)
 
     for (k = 0; k < layer_size; k++)
     {
-      if (fabs(m_array[(2*k + 1)*t]) <= threshold)        //fabs() вычисляет модуль числа (double)
+      if (Math.abs(m_array[(2*k + 1)*t]) <= threshold)      //fabs() вычисляет модуль числа (double)
         m_array[(2*k + 1)*t] = 0;
     }
   }
   else
   {
-    int layer_size = (1<<(n - Depth)) + 1;             //двойка в степени (n-Depth)
+    let layer_size = Math.pow(2, n - Depth) + 1;            //двойка в степени (n-Depth)
+    let t = Math.pow(2, Depth);                             //двойка в степени Depth
 
-    int t = 1<<Depth;                                            //двойка в степени Depth
-
-    if (fabs(m_array[k*t]) <= threshold)
+    if (Math.abs(m_array[k*t]) <= threshold)
       m_array[k*t] = 0;
   }
+
+  return m_array;
 }
